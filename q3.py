eef942ca666e0fa16bff9a532194385c5880f805
jelli vaishnavi <154883536+jellivaishnavi2004@users.noreply.github.com>
2024-09-30 10:35:27 +0530

    Update q3.py

--- q3.py
+++ q3.py
@@ -7,19 +7,19 @@ Refer sample input and output for formatting specifications.
 '''
 
 def display_match_details():
-    # Input for Team 1
+    
     print("Team 1:")
     team1_name = input("Team Name:\n")
     team1_score = input("Score:\n")
     team1_overs = input("Overs played:\n")
     
-    # Input for Team 2
+    
     print("Team 2:")
     team2_name = input("Team name:\n")
     team2_score = input("Score:\n")
     team2_overs = input("Overs played:\n")
     
-    # Displaying Match Details
+    
     print("Match Details:")
     print("Team 1:")
     print(f"Name: {team1_name}")
@@ -30,5 +30,5 @@ def display_match_details():
     print(f"Score: {team2_score}")
     print(f"Overs played: {team2_overs}")
 
-# Call the function to display match details
+
 display_match_details()
